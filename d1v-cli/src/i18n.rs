7e6dd694e813f3cc1dee13e2051dafcc6f3e6e28
fhluo <20843637+fhluo@users.noreply.github.com>
2026-03-22 22:45:02 +0800

    feat: add `lookup` function and `t!` macro

--- d1v-cli/src/i18n.rs
+++ d1v-cli/src/i18n.rs
@@ -1,3 +1,7 @@
+use std::borrow::Cow;
+use std::collections::HashMap;
+
+use fluent_templates::fluent_bundle::FluentValue;
 use fluent_templates::{static_loader, Loader};
 use parking_lot::RwLock;
 use tracing::debug;
@@ -12,6 +16,10 @@ static_loader! {
 }
 
 static LOCALE: RwLock<LanguageIdentifier> = RwLock::new(langid!("en"));
+
+pub fn locale() -> LanguageIdentifier {
+    LOCALE.read().clone()
+}
 
 pub fn init(locales: impl IntoIterator<Item = impl AsRef<str>>) {
     let locale = LocaleResolver::new().resolve(locales);
@@ -30,17 +38,15 @@ impl LocaleResolver {
         }
     }
 
-    fn resolve(&self, sources: impl IntoIterator<Item = impl AsRef<str>>) -> LanguageIdentifier {
-        sources
+    fn resolve(&self, locales: impl IntoIterator<Item = impl AsRef<str>>) -> LanguageIdentifier {
+        locales
             .into_iter()
             .filter_map(|s| s.as_ref().parse::<LanguageIdentifier>().ok())
             .find_map(|id| self.find_best(&id))
             .unwrap_or_else(|| langid!("en"))
     }
 
-    fn find_best(&self, requested: impl AsRef<LanguageIdentifier>) -> Option<LanguageIdentifier> {
-        let requested = requested.as_ref();
-
+    fn find_best(&self, requested: &LanguageIdentifier) -> Option<LanguageIdentifier> {
         self.available
             .iter()
             .find(|&id| id == requested)
@@ -51,4 +57,28 @@ impl LocaleResolver {
             })
             .cloned()
     }
+}
+
+pub fn lookup(id: &str, args: &[(&'static str, String)]) -> String {
+    let map = (!args.is_empty()).then(|| {
+        args.iter()
+            .map(|(k, v)| (Cow::Borrowed(*k), FluentValue::from(v.as_str())))
+            .collect()
+    });
+
+    LOCALES
+        .try_lookup_complete(&locale(), id, map.as_ref())
+        .unwrap_or_else(|| id.to_string())
+}
+
+#[macro_export]
+macro_rules! t {
+    ($id:expr) => {
+        $crate::i18n::lookup($id, &[])
+    };
+    ($id:expr, $($key:ident = $val:expr),+ $(,)?) => {{
+        $crate::i18n::lookup($id, &[
+            $( (stringify!($key), $val.to_string()), )+
+        ])
+    }};
 }
