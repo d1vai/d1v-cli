a052f7747b78a1b6ea5255ee42d7612878249d7c
fhluo <20843637+fhluo@users.noreply.github.com>
2026-04-04 09:52:04 +0800

    refactor: replace `inquire::Text` with `ui::Text`

--- d1v-cli/src/prompt.rs
+++ d1v-cli/src/prompt.rs
@@ -1,35 +1,25 @@
 use anyhow::Result;
 use d1v_api::{Code, Email, Validate};
-use inquire::validator::{ErrorMessage, Validation};
-use inquire::Text;
 use secrecy::SecretString;
 
 use crate::t;
-use crate::ui::Password;
-
-macro_rules! validator {
-    ($type:ident, $msg:expr) => {
-        |input: &str| -> Result<Validation, inquire::CustomUserError> {
-            Ok(if $type(input).validate().is_ok() {
-                Validation::Valid
-            } else {
-                Validation::Invalid(ErrorMessage::Custom($msg))
-            })
-        }
-    };
-}
+use crate::ui::{Password, Text};
 
 /// Prompts for an email address with format validation.
 pub fn email() -> Result<String> {
-    Ok(Text::new(&t!("auth-email-prompt"))
-        .with_validator(validator!(Email, t!("auth-email-invalid")))
+    Ok(Text::new(t!("auth-email-prompt"))
+        .with_validator(|input| {
+            Email(input)
+                .validate()
+                .map_err(|_| t!("auth-email-invalid"))
+        })
         .prompt()?)
 }
 
 /// Prompts for a 6-digit verification code with format validation.
 pub fn code() -> Result<String> {
-    Ok(Text::new(&t!("auth-code-prompt"))
-        .with_validator(validator!(Code, t!("auth-code-invalid")))
+    Ok(Text::new(t!("auth-code-prompt"))
+        .with_validator(|input| Code(input).validate().map_err(|_| t!("auth-code-invalid")))
         .prompt()?)
 }
 
